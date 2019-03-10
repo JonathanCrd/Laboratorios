b78a7e6c66f10c29620eaf8512064d2e9c09a097
Jonathan Cárdenas <jonym97156@gmail.com>
2019-03-10 14:49:56 -0600

    Geographic request done

--- Lab 4 - weather app/app.js
+++ Lab 4 - weather app/app.js
@@ -1,11 +1,9 @@
 const credentials = require('./credentials');
 const request = require('request');
 
-let city = "Monterrey, Nuevo Leon";
+let city = "Monterrey Nuevo Leon";
 //Latitud de Monterrey: 25.6714
 //Longitud de Monterrey:-100.309
-
-console.log(`EL CLIMA EN ${city}`);
 
 function getWeather(lat,long) {
   request.get(`https://api.darksky.net/forecast/${credentials.DARK_SKY_SECRET_KEY}/${lat},${long}?lang=es&units=si`,
@@ -17,7 +15,15 @@ function getWeather(lat,long) {
 }
 
 function getGeo(city_name) {
-  //Aqui se va a buscar el city name
+  request.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${city_name}.json?access_token=${credentials.MAPBOX_TOKEN}`,
+  function (error, response, body){
+    let jsonBody = JSON.parse(body);
+    console.log(jsonBody.features[0].center[0]); //long
+    console.log(jsonBody.features[0].center[1]); //lat
+    getWeather()
+  });
 }
 
-getWeather(25.6714,-100.309);
+console.log("CLIMA EN MONTERREY NUEVO LEON");
+getGeo(city);
+//getWeather(25.6714,-100.309);
