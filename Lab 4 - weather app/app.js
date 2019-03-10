f433c23e592cf84f02d83271c3aef8bc99096e5d
Jonathan Cárdenas <jonym97156@gmail.com>
2019-03-10 14:53:25 -0600

    Lab 4 finished

--- Lab 4 - weather app/app.js
+++ Lab 4 - weather app/app.js
@@ -5,6 +5,7 @@ let city = "Monterrey Nuevo Leon";
 //Latitud de Monterrey: 25.6714
 //Longitud de Monterrey:-100.309
 
+//Obtener los datos del clima del DIA (se uso Daily en vez de Current)
 function getWeather(lat,long) {
   request.get(`https://api.darksky.net/forecast/${credentials.DARK_SKY_SECRET_KEY}/${lat},${long}?lang=es&units=si`,
     function (error, response, body){
@@ -14,16 +15,19 @@ function getWeather(lat,long) {
   );
 }
 
+//Obtener las coordenadas geograficas de la ciudad
 function getGeo(city_name) {
   request.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${city_name}.json?access_token=${credentials.MAPBOX_TOKEN}`,
   function (error, response, body){
     let jsonBody = JSON.parse(body);
-    console.log(jsonBody.features[0].center[0]); //long
-    console.log(jsonBody.features[0].center[1]); //lat
-    getWeather()
+    let long = jsonBody.features[0].center[0];
+    let lat = jsonBody.features[0].center[1];
+
+    //Una vez que se tienen las coordenadas, podemos obtener los datos climaticos
+    getWeather(lat, long)
   });
 }
 
-console.log("CLIMA EN MONTERREY NUEVO LEON");
+
+console.log(`CLIMA EN ${city.toUpperCase()}`);
 getGeo(city);
-//getWeather(25.6714,-100.309);
