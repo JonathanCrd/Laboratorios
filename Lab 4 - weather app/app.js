726ffda2152ba4001cef9ce56a12908e2c546703
Jonathan Cárdenas <jonym97156@gmail.com>
2019-03-10 14:34:33 -0600

    Request of Weather fully implemented

--- Lab 4 - weather app/app.js
+++ Lab 4 - weather app/app.js
@@ -8,7 +8,12 @@ let city = "Monterrey, Nuevo Leon";
 console.log(`EL CLIMA EN ${city}`);
 
 function getWeather(lat,long) {
-  console.log(request.get(`https://api.darksky.net/forecast/${credentials.DARK_SKY_SECRET_KEY}/${lat},${long}?lang=es&units=si`));
+  request.get(`https://api.darksky.net/forecast/${credentials.DARK_SKY_SECRET_KEY}/${lat},${long}?lang=es&units=si`,
+    function (error, response, body){
+      let jsonRequest = JSON.parse(body);
+      console.log(`${jsonRequest.daily.data[0].summary} Actualmente está a ${jsonRequest.currently.temperature}° C. Hay ${jsonRequest.daily.data[0].precipProbability * 100}% de posibilidad de lluvia.`);
+    }
+  );
 }
 
 function getGeo(city_name) {
