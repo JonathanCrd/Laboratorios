dd6264ec7b709a29360e51f1a54418c6f90bfcc7
Jonathan Cárdenas <jonym97156@gmail.com>
2019-03-31 18:50:17 -0600

    First Try, added new app.js

--- Lab 6 - WebServer/app.js
+++ Lab 6 - WebServer/app.js
@@ -1,79 +1,49 @@
-const credentials = require('./credentials');
-const request = require('request');
+const path = require('path')
+const express = require('express')
+const weather = require('./weather.js')
 
-let city = "Monterrey Nuevo Leon";
-//Latitud de Monterrey: 25.6714
-//Longitud de Monterrey:-100.309
+const app = express()
 
-//Obtener los datos del clima del DIA (se uso Daily en vez de Current)
-function getWeather(lat,long,callback) {
-  request.get(`https://api.darksky.net/forecast/${credentials.DARK_SKY_SECRET_KEY}/${lat},${long}?lang=es&units=si`,
-    function (error, response, body){
-      //Error handling
-      if(error){
-        callback("Error, Servicio no disponible", undefined)
-      } else if(response.body == 'Forbidden\n'){
-        callback('Credenciales incorrectas',undefined)
-      } else if(response.body.code == 400){
-          callback(response.body.error, undefined);
-      } else if(response.body == 'Not Found\n'){
-        callback('Error, no se encontró el lugar',undefined)
-      }
-      //Everything is fine
-      else {
-        let jsonRequest = JSON.parse(body);
-        let weatherData = {
-          summary: jsonRequest.daily.data[0].summary,
-          temperature: jsonRequest.currently.temperature,
-          precipProbability : jsonRequest.daily.data[0].precipProbability
-        }
-        callback(undefined,weatherData);
-      }
-    }
-  );
-}
+const port = process.env.PORT || 3000;
 
-//Obtener las coordenadas geograficas de la ciudad
-function getGeo(city_name, callback) {
-  request.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${city_name}.json?access_token=${credentials.MAPBOX_TOKEN}`,
-  function (error, response, body){
-    let jsonBody = JSON.parse(body);
-    //Error handling
-    if (error){
-      callback("Error, servicio no disponible",undefined)
-    }else if(jsonBody.message == "Not Found"){
-      callback("No encontrado", undefined);
-    }else if(jsonBody.message == 'Not Authorized - Invalid Token'){
-      callback("Token Invalido", undefined);
-    }else if(jsonBody.features.length == 0){
-          callback("Error, lugar no encontrado", undefined)
-    }
-    //Everything is fine
-    else{
-      let coordinates = {
-        long: jsonBody.features[0].center[0],
-        lat: jsonBody.features[0].center[1]
-      }
-      callback(undefined, coordinates)
-    }
-  });
-}
-
-
-console.log(`CLIMA EN ${city.toUpperCase()}`);
-
-getGeo(city, function(error,response){
-  if(error){
-    console.log(error);
-  }
-  else{
-    //Una vez que se tienen las coordenadas, podemos obtener los datos climaticos
-    getWeather(response.lat, response.long, function(error, response){
-      if(error){
-        console.log(error);
-      }else{
-        console.log(`${response.summary} Actualmente está a ${response.temperature}° C. Hay ${response.precipProbability * 100}% de posibilidad de lluvia.`);
-      }
-    });
-  }
+app.get('/', function(req, res) {
+   res.send('<h1>Hola mundo!</h1>')
 });
+
+app.get('/weather', function(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  if( !req.query.search ) {
+    return res.send({
+      error: 'Tienes que dar una ciudad a buscar'
+    })
+  }
+  weather.weather(req.query.search, function(error, response) {
+    if(error) {
+      return res.send({
+        error: error
+      })
+    }
+    //const movie = response
+      res.send({
+        response
+        /*
+        title: movie.title,
+        plot: movie.plot,
+        rating: movie.rating
+        */
+      })
+    }
+  )
+})
+
+
+app.get('*', function(req, res) {
+  res.send({
+    error: 'Esta ruta no existe'
+  })
+})
+
+
+app.listen(port, function() {
+  console.log('up and running')
+})
